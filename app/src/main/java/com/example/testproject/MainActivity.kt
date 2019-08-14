919a04e5da5349d208e595d06d5ac8f83dde7c62
eugeniapotapska <epotapska1@logitech.com>
2019-08-14 12:49:21 +0300

    Add AppCenter support

--- app/src/main/java/com/example/testproject/MainActivity.kt
+++ app/src/main/java/com/example/testproject/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.testproject
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCenter.start(
+            application, "5c0bdb41-4736-41fe-8150-1e724be8cff8",
+            Analytics::class.java, Crashes::class.java
+        )
     }
 }
